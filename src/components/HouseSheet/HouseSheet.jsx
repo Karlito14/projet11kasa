46075e611f4bdb45e1a55d747fb714db4fa3ed7f
Karlito14 <leiroz26@hotmail.com>
2024-04-24 15:13:23 +0200

    affichae des images

--- src/components/HouseSheet/HouseSheet.jsx
+++ src/components/HouseSheet/HouseSheet.jsx
@@ -6,12 +6,30 @@ export const HouseSheet = ({ house }) => {
     const [indexImage, setIndexImage] = useState(0);
 
     const { description, pictures, host, equipments, location, title, rating, tags} = house;
-    console.log(house)
+
+    const incrIndex = () => {
+        let newIndex = indexImage + 1;
+        if(newIndex > pictures.length - 1) {
+            newIndex = 0;
+        }
+        setIndexImage(newIndex);
+    };
+
+    const decrIndex = () => {
+        let newIndex = indexImage - 1;
+        if(newIndex < 0) {
+            newIndex = pictures.length - 1;
+        }
+        setIndexImage(newIndex);
+    };
+    
     return (
         <main className={style.main}>
             <div className={style.containerImg}>
-                {<IoIosArrowBack />}
-                {<IoIosArrowForward />}
+                {pictures.length > 1 && <IoIosArrowBack className={style.containerImg__iconPrev} onClick={() => decrIndex()} />}
+                <img src={pictures[indexImage]} alt={title} className={style.containerImg__img} />
+                {pictures.length > 1 && <span className={style.containerImg__span}>{indexImage + 1}/{pictures.length}</span>}
+                {pictures.length > 1 && <IoIosArrowForward className={style.containerImg__iconNext} onClick={() => incrIndex()} />}
             </div>
         </main>
     );
