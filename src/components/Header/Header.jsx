fee57247c5283fb25afc984700fb2acc2e01c131
Karlito14 <leiroz26@hotmail.com>
2024-04-29 19:03:15 +0200

    semantique navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,19 +8,27 @@ export const Header = ({ updatePage, currentPage }) => {
     return (
         <header className={style.container}>
             <img className={style.container__logo} src={logo} alt='Kasa' />
-            <nav className={style.container__nav}>
-                <Link 
-                    className={style.container__nav__link} 
-                    to='/' 
-                    style={{textDecoration: currentPage === 'home' ? 'underline' : ''}}
-                    onClick={() => updatePage('home')}
-                >Accueil</Link>
-                <Link 
-                    className={style.container__nav__link} 
-                    to='/about'
-                    style={{textDecoration: currentPage === 'about' ? 'underline' : ''}}
-                    onClick={() => updatePage('about')}
-                >A propos</Link>
+            <nav>
+                <ul className={style.container__nav}>
+                    <li>
+                        <Link 
+                            className={style.container__nav__link} 
+                            to='/' 
+                            style={{textDecoration: currentPage === 'home' ? 'underline' : ''}}
+                            onClick={() => updatePage('home')}
+                        >Accueil</Link>
+                    </li>
+                    <li>
+                        <Link 
+                            className={style.container__nav__link} 
+                            to='/about'
+                            style={{textDecoration: currentPage === 'about' ? 'underline' : ''}}
+                            onClick={() => updatePage('about')}
+                        >A propos</Link>
+                    </li>
+                </ul>
+                
+                
             </nav>
         </header>
     );
